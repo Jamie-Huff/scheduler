f3f03fbe37539d80c2456c47dcdb4459f928649f
Jamie <jamiehuff737@gmail.com>
2021-08-04 17:56:13 -0400

    first commit

---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+const formatSpots = (spots) => {
+  return spots === 0 ? `no spots remaining` : spots === 1 ? `1 spot remaining`:`${spots} spots remaining`
+}
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames('day-list__item', {
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2> 
+      <h3>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewerMapper = props.interviewers.map((interviewerPerson, index) => {
+    const isSelected = props.interviewer === interviewerPerson.id ? true : false 
+    return (
+      <InterviewerListItem
+        key={interviewerPerson.id} 
+        name={interviewerPerson.name} 
+        avatar={interviewerPerson.avatar}
+        selected={isSelected}
+        setInterviewer={event => props.setInterviewer(interviewerPerson.id)}
+      />
+    )
+  })
+
+
+  return (
+    <ul>
+      {interviewerMapper}
+    </ul>
+  )
+}
